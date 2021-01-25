ad116c2e057c4085d8390f635e76ece9e21cf128
Jeremiah Le <jeremiah.le@snhu.edu>
2021-01-24 18:08:35 -0600

    Lab 1 - Initial Commit

---
+++ Lab 1/Lab 1/GAM345Vector.h
@@ -0,0 +1,34 @@
+#pragma once
+namespace GAM345 {
+
+	template<typename T>
+	class Vector {
+		public:
+			typename<T>::Vector() {
+				size = 0;
+				capacity = 10;
+			}
+
+			typename<T>::~Vector() {
+
+			}
+
+			typename<T>::push_back(T) {
+
+			}
+
+			typename<T>::resize(T) {
+
+			}
+
+			typename<T>::reserve(T) {
+
+			}
+
+		private:
+			int size;
+			int capacity;
+			int* data;
+	};
+
+}
