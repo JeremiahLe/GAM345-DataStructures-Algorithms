ffe999fe6206b6475048722d1d20c28ebfd1a202
Jeremiah Le <jeremiah.le@snhu.edu>
2021-02-21 15:16:00 -0600

    I ACTUALLY DID IT

--- Search and Sort/Search and Sort/Search and Sort.cpp
+++ Search and Sort/Search and Sort/Search and Sort.cpp
@@ -12,7 +12,7 @@ int main()
 	int *p;
 	p = array;
 
-	p = &array[0]; *p = 0;
+	p = &array[0]; *p = 5;
 	p = &array[1]; *p = 2;
 	p = &array[2]; *p = 4;
 	p = &array[3]; *p = 3;
@@ -29,6 +29,11 @@ int main()
 		std::cout << array[i] << ", ";
 
 	GAM345::SequentialSearch(array, 5, 1); // value "1" not found!
+
+	GAM345::InsertionSort(array, 5);
+
+	for (int i = 0; i < 5; i++)
+		std::cout << array[i] << ", ";
 
 };
 
--- Search and Sort/Search and Sort/Search and Sort.h
+++ Search and Sort/Search and Sort/Search and Sort.h
@@ -4,20 +4,36 @@
 namespace GAM345 {
 
 	bool SequentialSearch(int *aArray, int aSize, int aValue) {
-		if (aSize == 0) // faulty array?
-			return false; 
+		if (aSize == 0) { // empty array?
+			return false;
+			std::cout << "Invalid array" << std::endl;
+		}
 		for (int i = 0; i < aSize; i++) { // find value
 			if (aArray[i] == aValue) {
 				std::cout << "Value " << aValue << " found!" << std::endl;
 				return true;
 			}
 		}
-			std::cout << "Value " << aValue << " not found!" << std::endl;
+			std::cout << "Value " << aValue << " not found!" << std::endl; // value doesn't exist
 			return false;
 	}
 
 	void InsertionSort(int *aArray, int aSize) {
-		// pass-by value
+		if (aSize == 0) // empty array?
+			return;
+		int index = 1;
+		for (int i = 0; i < aSize; i++) {
+			while (index < aSize) {
+				if (aArray[index] < aArray[index - 1]) { // is second index smaller than first? if yes than swap // 5, 2, 4, 3, 6 // is 2 < 5? yes, 
+					int temp = aArray[index - 1]; // value to be moved over to the left
+					aArray[index - 1] = aArray[index]; // left value to right value
+					aArray[index] = temp; // right value to left value
+					index--; // move back
+				}
+				else
+					index++;
+			}
+		}
 	}
 
 };
