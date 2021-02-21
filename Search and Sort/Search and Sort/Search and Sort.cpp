f7b331c233031d65b4492609df6644d455fb065d
Jeremiah Le <jeremiah.le@snhu.edu>
2021-02-21 17:15:00 -0600

    fixed memory issue with edge case

--- Search and Sort/Search and Sort/Search and Sort.cpp
+++ Search and Sort/Search and Sort/Search and Sort.cpp
@@ -8,14 +8,15 @@
 
 int main()
 {
+	
 	int array[5];
 	int *p;
 	p = array;
 
-	p = &array[0]; *p = 5;
-	p = &array[1]; *p = 2;
-	p = &array[2]; *p = 4;
-	p = &array[3]; *p = 3;
+	p = &array[0]; *p = 17;
+	p = &array[1]; *p = 8;
+	p = &array[2]; *p = 25;
+	p = &array[3]; *p = 10;
 	p = &array[4]; *p = 1; // value 1 is here
 
 	for (int i = 0; i < 5; i++)
@@ -34,6 +35,20 @@ int main()
 
 	for (int i = 0; i < 5; i++)
 		std::cout << array[i] << ", ";
+	
+	
+	int newArray[2];
+	int*ptr;
+	ptr = newArray;
+
+	ptr = &newArray[0]; *ptr = 1;
+	ptr = &newArray[1]; *ptr = 0;
+
+	GAM345::InsertionSort(newArray, 2); 
+
+	for (int i = 0; i < 2; i++)
+		std::cout << newArray[i] << ", "; 
+		
 
 };
 
--- Search and Sort/Search and Sort/Search and Sort.h
+++ Search and Sort/Search and Sort/Search and Sort.h
@@ -22,9 +22,8 @@ namespace GAM345 {
 		if (aSize == 0) // empty array?
 			return;
 		int index = 1;
-		for (int i = 0; i < aSize; i++) {
 			while (index < aSize) {
-				if (aArray[index] < aArray[index - 1]) { // is second index smaller than first? if yes than swap // 5, 2, 4, 3, 6 // is 2 < 5? yes, 
+				if (aArray[index] < aArray[index - 1] && index >= 1) { // is second index smaller than first?
 					int temp = aArray[index - 1]; // value to be moved over to the left
 					aArray[index - 1] = aArray[index]; // left value to right value
 					aArray[index] = temp; // right value to left value
@@ -34,7 +33,5 @@ namespace GAM345 {
 					index++;
 			}
 		}
-	}
-
 };
 
