ff8e8132da17129da791518953958766236ec892
Jeremiah Le <jeremiah.le@snhu.edu>
2021-04-10 21:17:30 -0500

    MergeSort helper function fix + Finished Quicksort vs MergeSort tests

--- Search and Sort/Search and Sort/Search and Sort.cpp
+++ Search and Sort/Search and Sort/Search and Sort.cpp
@@ -4,10 +4,16 @@
 #include <iostream>
 #include <vector>
 #include "Search and Sort.h"
+#include <Windows.h>
+#include <sysinfoapi.h>
 
 
 int main()
 {
+	GAM345Namespace::GAM345 myClass;
+
+	// Sequential Search //
+
 	std::cout << "Sequential Search:" << std::endl;
 
 	int array[6];
@@ -25,40 +31,49 @@ int main()
 	for (int i = 0; i < 6; i++)
 		std::cout << array[i] << " ";
 
-	GAM345::SequentialSearch(array, 6, 1); // value "1" found!
+	myClass.GAM345::SequentialSearch(array, 6, 1); // value "1" found!
 
 	p = &array[4]; *p = 6; // change address holding 1 to something else
 
 	for (int i = 0; i < 6; i++)
 		std::cout << array[i] << " ";
 
-	GAM345::SequentialSearch(array, 6, 1); // value "1" not found!
+	myClass.GAM345::SequentialSearch(array, 6, 1); // value "1" not found!
+
+	// Insertion Sort //
 
 	std::cout << std::endl;
 
-	GAM345::InsertionSort(array, 6);
+	myClass.GAM345::InsertionSort(array, 6);
 
 	std::cout << "Insertion Sort: ";
 
 	for (int i = 0; i < 6; i++)
 		std::cout << array[i] << " ";
 
+	// Merge Sort //
+
 	std::cout << std::endl;
 
+	int mergeSortArray[] = { 7, 6, 4, 3, 5};
 	std::cout << "Merge Sort: ";
 
-	GAM345::MergeSort(array, 6);
+	myClass.GAM345::MergeSort(mergeSortArray, 5);
 
-	for (int i = 0; i < 6; i++)
-		std::cout << array[i] << " ";
+	for (int i = 0; i < 5; i++)
+		std::cout << mergeSortArray[i] << " ";
 
 	std::cout << std::endl;
+
+	// Binary Search //
+
 	std::cout << std::endl;
+
 	std::cout << "Binary Search:" << std::endl;
 
 	int newArray[] = { 1, 2, 3, 4, 5, 6 };
 	int value = 6;
-	int bsResult = GAM345::BinarySearch(newArray, 6, value);
+	int bsResult = myClass.GAM345::BinarySearch(newArray, 6, value);
 
 	for (int i = 0; i < 6; i++)
 		std::cout << newArray[i] << " ";
@@ -71,7 +86,7 @@ int main()
 		std::cout << "Value " << value << " found at index " << bsResult;
 
 	value = 20;
-	bsResult = GAM345::BinarySearch(newArray, 6, value);
+	bsResult = myClass.GAM345::BinarySearch(newArray, 6, value);
 
 	std::cout << std::endl;
 
@@ -86,6 +101,9 @@ int main()
 		std::cout << "Value " << value << " found at index " << bsResult;
 
 	std::cout << std::endl;
+
+	// Quick Sort //
+
 	std::cout << std::endl;
 
 	int newArray2[] = { 1, 20, 14, 56, 44, 6 };
@@ -98,13 +116,53 @@ int main()
 
 	std::cout << std::endl;
 
-	GAM345::QuickSort(newArray2, 0, 5);
+	myClass.GAM345::QuickSort(newArray2, 0, 5);
 
 	std::cout << "After QuickSort: ";
 	for (int i = 0; i < 6; i++)
 		std::cout << newArray2[i] << " ";
 
 	std::cout << std::endl;
+	std::cout << std::endl;
+
+	
+	////////////////////////////////////////////////////////////// Mergesort vs Quicksort Testing ////////////////////////////////////
+
+	std::cout << "Mergesort vs Quicksort Testing:" << std::endl;
+
+	std::cout << std::endl;
+
+	unsigned long long startTime;
+	unsigned long long endTime;
+
+	startTime = GetTickCount64();
+
+	 // 10,000 randomized unsorted elements - quicksort
+	const int iterativeSize = 20000;
+	int randomArray[iterativeSize];
+	int sortedArray[iterativeSize];
+
+	for (int i = 0; i < iterativeSize; i++)
+		randomArray[i] = rand()%100;
+	for (int i = 0; i < iterativeSize; i++)
+		sortedArray[i] = 0 + i;
+
+	myClass.GAM345::QuickSort(sortedArray, 0, iterativeSize - 1);
+
+	for (int i = 0; i < iterativeSize; i++)
+		std::cout << sortedArray[i] << " ";
+	
+
+	endTime = GetTickCount64();
+
+	std::cout << std::endl;
+
+	double elapsedTime = (endTime - startTime)*1e-3;
+
+	std::cout << "Elapsed Time: " << elapsedTime << std::endl;
+
+
+	
 };
 
 
--- Search and Sort/Search and Sort/Search and Sort.h
+++ Search and Sort/Search and Sort/Search and Sort.h
@@ -1,27 +1,30 @@
 #pragma once
 #include <iostream>
 
-namespace GAM345 {
+namespace GAM345Namespace {
 
-	bool SequentialSearch(int *aArray, int aSize, int aValue) {
-		if (aSize == 0) { // empty array?
-			return false;
-			std::cout << "Invalid array" << std::endl;
-		}
-		for (int i = 0; i < aSize; i++) { // find value
-			if (aArray[i] == aValue) {
-				std::cout << "Value " << aValue << " found!" << std::endl;
-				return true;
+	class GAM345 {
+
+	public: 
+		bool SequentialSearch(int *aArray, int aSize, int aValue) {
+			if (aSize == 0) { // empty array?
+				return false;
+				std::cout << "Invalid array" << std::endl;
 			}
-		}
+			for (int i = 0; i < aSize; i++) { // find value
+				if (aArray[i] == aValue) {
+					std::cout << "Value " << aValue << " found!" << std::endl;
+					return true;
+				}
+			}
 			std::cout << "Value " << aValue << " not found!" << std::endl; // value doesn't exist
 			return false;
-	}
+		}
 
-	void InsertionSort(int *aArray, int aSize) {
-		if (aSize == 0) // empty array?
-			return;
-		int index = 1;
+		void InsertionSort(int *aArray, int aSize) {
+			if (aSize == 0) // empty array?
+				return;
+			int index = 1;
 			while (index < aSize) {
 				if (aArray[index] < aArray[index - 1] && index >= 1) { // is second index smaller than first?
 					int temp = aArray[index - 1]; // value to be moved over to the left
@@ -34,184 +37,267 @@ namespace GAM345 {
 			}
 		}
 
-	void merge(int* leftArray, int leftSize, int* rightArray, int rightSize) {
+		/*
+		void merge(int* leftArray, int leftSize, int* rightArray, int rightSize) {
 
-		int* answer = new int[leftSize + rightSize];
+			int* answer = new int[leftSize + rightSize]; // 3 + 3 = 6
 
-		// Loop through both left and right at same time, taking smaller element from each side, 
-		// until we reach end of one the arrays
+			//int* L = new int[leftSize];
+			//int* R = new int[rightSize];
 
-		int leftIndex = 0;
-		int rightIndex = 0;
-		int answerIndex = 0;
+			// Loop through both left and right at same time, taking smaller element from each side,
+			// until we reach end of one the arrays
 
-		while (leftIndex < leftSize && rightIndex < rightSize) {  
-			if (leftArray[leftIndex] < rightArray[rightIndex]) { // if left array index > right array index, add it to answer
-				answer[answerIndex] = leftArray[leftIndex]; 
+			int leftIndex = 0;
+			int rightIndex = 0;
+			int answerIndex = 0;
+
+			//for (int i = 0; i < leftSize; i++)
+				//L[i] = leftArray[i];
+			//for (int j = 0; j < rightSize; j++)
+				//R[j] = rightArray[j];
+
+			// Merge Arrays
+
+			/*
+			while (leftIndex < leftSize && rightIndex < rightSize) {
+				if (leftArray[leftIndex] <= rightArray[rightIndex]) {
+				a
+			}
+
+
+			while (leftIndex < leftSize && rightIndex < rightSize) {  // while 0 < 3 && 0 < 3
+				if (leftArray[leftIndex] < rightArray[rightIndex]) { // if left array index > right array index, add it to answer
+					answer[answerIndex] = leftArray[leftIndex];
+					leftIndex++;
+					answerIndex++;
+				}
+				else {
+					answer[answerIndex] = rightArray[rightIndex]; // else add the right array index to answer
+					rightIndex++;
+					answerIndex++;
+				}
+				//answerIndex++;
+			}
+
+			// check if there's remaining data in left - add it to answer
+			while (leftIndex < leftSize) {
+				answer[answerIndex] = leftArray[leftIndex];
 				leftIndex++;
 				answerIndex++;
 			}
-			else {
-				answer[answerIndex] = rightArray[rightIndex]; // else add the right array index to answer
+
+
+			// check if there's remaining data in right - add it to answer
+			while (rightIndex < rightSize) {
+				answer[answerIndex] = rightArray[rightIndex];
 				rightIndex++;
 				answerIndex++;
 			}
-		}
 
-		// check if there's remaining data in left - add it to answer
-		while (leftIndex < leftSize) {
-			answer[answerIndex] = leftArray[leftIndex];
-			leftIndex++;
-			answerIndex++;
-		}
-
-		// check if there's remaining data in right - add it to answer
-		while (rightIndex < rightSize) {
-			answer[answerIndex] = rightArray[rightIndex];
-			rightIndex++;
-			answerIndex++;
-		}
-
-		// copy data from answer into array Left
-		int i = 0;
-		while (i < (leftSize + rightSize)) {
-			leftArray[i] = answer[i];
-			i++;
-		}
-	}
-
-	void MergeSort(int *aArray, int aSize) {
-		if (aSize <= 1)
-			return;
-		if (aSize > 2) {
-			// Divide the data in half
-			int halfSize = aSize / 2;
-
-			// Sort the left side
-			MergeSort(aArray, halfSize);
-
-			// Sort the right side
-			MergeSort(aArray + halfSize, halfSize);
-
-			// Merge the data together
-			merge(aArray, halfSize, aArray + halfSize, halfSize);
-		}
-	}
-
-
-	int BinarySearch(int *aArray, int aSize, int value){
-
-		int left = aArray[0]; // first element
-		int right = aArray[aSize - 1]; // last element
-
-		while (left <= right) {
-
-			int midpoint = left + (right - left) / 2; // split the data in half
-
-			if (aArray[midpoint] == value) // if the current middle index is the value, return index
-				return midpoint;
-
-			if (aArray[midpoint] < value) // otherwise, if value is greater than current middle index, check right side
-				left = midpoint + 1;
-			else
-				right = midpoint - 1; // value is smaller than current middle index, check left side
-		}
-		return -1; // value not found
-	}
-	
-
-	int Partition(int *aArray, int low, int high) {
-
-		// 3, 6, 5, 1, 2, 4
-
-		int pivot = aArray[high]; // set the pivot to the last element // 4
-		int pivotIndex = high; // last index // 5
-		int i = low; // while loop
-
-		while (i < pivotIndex) { // while 0 < 5
- 			if (aArray[i] < pivot) {
-				i++; // number doesn't need to be swapped
+			// copy data from answer into array Left
+			int i = 0;
+			while (i < answerIndex) {
+				leftArray[i] = answer[i];
+				i++;
 			}
-			else // current array[i] > pivot (6 > 4)
+
+		}
+		*/
+
+		void merge(int *aArray, int left, int midpoint, int right) {
+
+			int leftArraySize = midpoint - left + 1;
+			int rightArraySize = right - midpoint;
+
+			int* leftArray = new int[leftArraySize];
+			int* rightArray = new int[rightArraySize];
+
+			// temp arrays data copy
+			for (int i = 0; i < leftArraySize; i++) {
+				leftArray[i] = aArray[left + i];
+			}
+			for (int j = 0; j < rightArraySize; j++) {
+				rightArray[j] = aArray[midpoint + 1 + j];
+			}
+
+			int leftIndex = 0;
+			int rightIndex = 0;
+			int answerIndex = left;
+
+			// Loop through both left and right at same time, taking smaller element from each side,
+			// until we reach end of one the arrays
+
+			while (leftIndex < leftArraySize && rightIndex < rightArraySize) {
+				if (leftArray[leftIndex] <= rightArray[rightIndex]) {
+					aArray[answerIndex] = leftArray[leftIndex];
+					leftIndex++;
+					answerIndex++;
+				}
+				else {
+					aArray[answerIndex] = rightArray[rightIndex];
+					rightIndex++;
+					answerIndex++;
+				}
+			}
+
+			// check if there's remaining data in left - add it to answer
+			while (leftIndex < leftArraySize) {
+				aArray[answerIndex] = leftArray[leftIndex];
+				leftIndex++;
+				answerIndex++;
+			}
+
+			// check if there's remaining data in right - add it to answer
+			while (rightIndex < rightArraySize) {
+				aArray[answerIndex] = rightArray[rightIndex];
+				rightIndex++;
+				answerIndex++;
+			}
+
+		}
+
+		void MergeSort(int *aArray, int aSize) {
+
+			MergeSort(aArray, 0, aSize - 1); // call helper function 
+
+		}
+
+		void MergeSort(int *aArray, int left, int right) {
+
+			if (left >= right) {
+				return; // base case
+			}
+
+			// where to split array
+			int midpoint = left + (right - left) / 2;
+
+			// call mergesort on left
+			MergeSort(aArray, left, midpoint);
+
+			// call mergesort on right
+			MergeSort(aArray, midpoint + 1, right);
+
+			// merge back together
+			merge(aArray, left, midpoint, right);
+
+		}
+
+
+		int BinarySearch(int *aArray, int aSize, int value) {
+
+			int left = aArray[0]; // first element
+			int right = aArray[aSize - 1]; // last element
+
+			while (left <= right) {
+
+				int midpoint = left + (right - left) / 2; // split the data in half
+
+				if (aArray[midpoint] == value) // if the current middle index is the value, return index
+					return midpoint;
+
+				if (aArray[midpoint] < value) // otherwise, if value is greater than current middle index, check right side
+					left = midpoint + 1;
+				else
+					right = midpoint - 1; // value is smaller than current middle index, check left side
+			}
+			return -1; // value not found
+		}
+
+
+		int Partition(int *aArray, int low, int high) {
+
+			// 3, 6, 5, 1, 2, 4
+
+			int pivot = aArray[high]; // set the pivot to the last element // 4
+			int pivotIndex = high; // last index // 5
+			int i = low; // while loop
+
+			while (i < pivotIndex) { // while 0 < 5
+				if (aArray[i] < pivot) {
+					i++; // number doesn't need to be swapped
+				}
+				else // current array[i] > pivot (6 > 4)
+				{
+					// 3 way swap
+					int lastPivotIndex = pivotIndex; // keep track of where the pivot was // lastpivotindex = [5]
+					int kickedToLeft = aArray[pivotIndex - 1]; // keep track of number that is replaced by pivot // int kickedtoleft = 2 ([4])
+
+					aArray[pivotIndex - 1] = aArray[pivotIndex]; // move the pivot left one //  2 [4] = 4 [5] // 3, -6-, 5, 1, 4, [4]
+					aArray[lastPivotIndex] = aArray[i]; // move the larger number to where the pivot was // 4 [5] = 6 [1] // 3, -6-, 5, 1, [4], -6-
+					aArray[i] = kickedToLeft; // kick the number replaced by the pivot all the way down to where the larger number was // 6 [1] = 2 [4] // 3, 2, 5, 1, [4], -6-
+					i++; // 3, 2, 5, 1, [4], 6
+
+					// 2 way quick swap
+					if (aArray[i] < aArray[i - 1]) { // if 2 < 3
+						int temp = aArray[i]; // set temp to [1] (2)
+						aArray[i] = aArray[i - 1]; // set [1] to [1 - 1] (3)
+						aArray[i - 1] = temp; // set [0] to temp (2)
+					}
+				}
+			}
+			return pivotIndex;
+		}
+
+		void QuickSort(int *aArray, int low, int high) {
+			if (low < high) { // base case
+
+				int partIndex = Partition(aArray, low, high);
+
+				QuickSort(aArray, partIndex + 1, high);
+
+				QuickSort(aArray, low, partIndex - 1);
+
+			}
+		}
+
+		/*
+		void mergeAbilities(AbilityChain *first, AbilityChain *second) {
+
+			int firstIndex = 0;
+			int secondIndex = 0;
+
+			AbilityChain* currentFirst = first;
+			AbilityChain* currentSecond = second;
+
+			while (currentFirst != nullptr && currentSecond != nullptr)
 			{
-				// 3 way swap
-				int lastPivotIndex = pivotIndex; // keep track of where the pivot was // lastpivotindex = [5]
-				int kickedToLeft = aArray[pivotIndex - 1]; // keep track of number that is replaced by pivot // int kickedtoleft = 2 ([4])
+				if (currentFirst->level < currentSecond->level) {
+					first[firstIndex] = currentFirst->level;
+					firstIndex++;
+					currentFirst = currentFirst->next;
+				}
+				else
+				{
+					second[secondIndex] = currentSecond->level;
+					secondIndex++;
+					currentSecond = currentSecond->next;
+				}
 
-				aArray[pivotIndex - 1] = aArray[pivotIndex]; // move the pivot left one //  2 [4] = 4 [5] // 3, -6-, 5, 1, 4, [4]
-				aArray[lastPivotIndex] = aArray[i]; // move the larger number to where the pivot was // 4 [5] = 6 [1] // 3, -6-, 5, 1, [4], -6-
-				aArray[i] = kickedToLeft; // kick the number replaced by the pivot all the way down to where the larger number was // 6 [1] = 2 [4] // 3, 2, 5, 1, [4], -6-
-				i++; // 3, 2, 5, 1, [4], 6
+			}
+		}
+		*/
 
-				// 2 way quick swap
-				if (aArray[i] < aArray[i - 1]) { // if 2 < 3
-					int temp = aArray[i]; // set temp to [1] (2)
-					aArray[i] = aArray[i - 1]; // set [1] to [1 - 1] (3)
-					aArray[i - 1] = temp; // set [0] to temp (2)
+		/*
+		void MergeAbility(int *aArray, int *Array2, int aSize, int aSize2) {
+
+			int firstIndex = 0;
+			int secondIndex = 0;
+
+			while (firstIndex < aSize && secondIndex < aSize2) {
+				if (aArray[firstIndex] < Array2[secondIndex]) {
+					aArray[firstIndex] = aArray[firstIndex];
+					firstIndex++;
+				}
+				else
+				{
+					aArray[firstIndex] = Array2[secondIndex];
+					secondIndex++;
 				}
 			}
 		}
-		return pivotIndex;
-	}
+		*/
 
-	void QuickSort(int *aArray, int low, int high) {
-		if (low < high) { // base case
-
-			int partIndex = Partition(aArray, low, high);
-
-			QuickSort(aArray, partIndex + 1, high);
-
-			QuickSort(aArray, low, partIndex - 1);
-
-		}
-	}
-
-	/*
-	void mergeAbilities(AbilityChain *first, AbilityChain *second) {
-
-		int firstIndex = 0;
-		int secondIndex = 0;
-
-		AbilityChain* currentFirst = first;
-		AbilityChain* currentSecond = second;
-
-		while (currentFirst != nullptr && currentSecond != nullptr)
-		{
-			if (currentFirst->level < currentSecond->level) {
-				first[firstIndex] = currentFirst->level;
-				firstIndex++;
-				currentFirst = currentFirst->next;
-			}
-			else
-			{
-				second[secondIndex] = currentSecond->level;
-				secondIndex++;
-				currentSecond = currentSecond->next;
-			}
-
-		}
-	}
-	*/
-
-	/*
-	void MergeAbility(int *aArray, int *Array2, int aSize, int aSize2) {
-
-		int firstIndex = 0;
-		int secondIndex = 0;
-
-		while (firstIndex < aSize && secondIndex < aSize2) {
-			if (aArray[firstIndex] < Array2[secondIndex]) {
-				aArray[firstIndex] = aArray[firstIndex];
-				firstIndex++;
-			}
-			else
-			{
-				aArray[firstIndex] = Array2[secondIndex];
-				secondIndex++;
-			}
-		}
-	}
-	*/
-	
-};
+	};
+}
 
